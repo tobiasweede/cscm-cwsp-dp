cc074e7fc5d4038fcada4b44943db0693f5bc2eb
Tobias Weiss <tobias@tobias-weiss.org>
2020-07-20 12:52:19 +0200

    autocommit

---
+++ IPSFramework/main.vb
@@ -0,0 +1,17 @@
+﻿Module main
+    Public OrderSequence() As Integer
+    Public SKUSequenz() As Integer
+
+
+    Sub Main()
+
+        For Each foundFile As String In My.Computer.FileSystem.GetFiles(CurDir() & "\Data\") 'batch load
+            Dim NewInstance As New Instance
+
+            NewInstance.LoadInst(foundFile)
+            Debug.Print("hallo Welt")
+        Next
+
+    End Sub
+
+End Module
