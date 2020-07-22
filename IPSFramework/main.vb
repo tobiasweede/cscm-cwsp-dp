edeec0a5bdbce524703cb7740d94bc3810300ed5
Tobias Weiss <tobias@tobias-weiss.org>
2020-07-22 21:34:29 +0200

    autocommit

--- IPSFramework/main.vb
+++ IPSFramework/main.vb
@@ -25,7 +25,7 @@
             Requests = New List(Of List(Of Integer)) ' Matrix (Als Liste von Listen realisiert, da effizienter) der (Essensindex, Tresen) Zuordnungen
             For Each kunde In C
                 Dim KundenBestellung As List(Of Integer) = New List(Of Integer) 'Liste mit Tresennummer mit für d_ik
-                Dim PosKundenBestellung As Integer = 1 'Posiion der Bestellung bzgl. des betrachteten Kunden
+                Dim PosKundenBestellung As Integer = 0 'Posiion der Bestellung bzgl. des betrachteten Kunden
                 For Each bestellung In NewInstance.Order(kunde).OrderSKU
                     Dim zuordnung As Tuple(Of Integer, Integer) = New Tuple(Of Integer, Integer)(kunde, PosKundenBestellung)
                     KundenBestellung.Add(bestellung)
@@ -42,7 +42,7 @@
                 If i = 0 Then ' Virtueller initStep
                     Dim initStep As SolutionStep = New SolutionStep
                     For j = 0 To n - 1 ' Initialisiere Kundenpositionen
-                        initStep.kundenPositionen.Add(-i)
+                        initStep.kundenPositionen.Add(-j)
                     Next
                     stage = New Dictionary(Of Integer, SolutionStep)
                     stage.Add(initStep.teilZFW, initStep) ' Füge initStep zur Stage hinzu
@@ -55,12 +55,13 @@
                     For Each request In feasibleRequests
                         Dim actualStep As SolutionStep = New SolutionStep(previousStepKVPair.Value)
                         actualStep.servedRequests.Add(request)
-                        For Each pos In actualStep.kundenPositionen
-                            pos += Requests(request.Item1)(request.Item2) - previousStepKVPair.Value.kundenPositionen(request.Item1)
+                        Dim movementDistance As Integer = Requests(request.Item1)(request.Item2) - actualStep.kundenPositionen(request.Item1)
+                        For pos As Integer = 0 To actualStep.kundenPositionen.Count - 1
+                            actualStep.kundenPositionen(pos) += movementDistance
                         Next
-                        actualStep.waiterPosition = request.Item1
+                        actualStep.waiterPosition = Requests(request.Item1)(request.Item2)
                         addStepTime(request, previousStepKVPair.Value, actualStep) ' Füge delta(Z,Z') hinzu
-                        stage.Add(actualStep.teilZFW, actualStep) ' Füge initStep zur Stage hinzu
+                        stage.Add(actualStep.teilZFW, actualStep) ' Füge actualStep zur Stage hinzu
                     Next
                 Next
                 stages.Add(stage) ' Füge Stage zu Liste der Stages hinzu
@@ -69,11 +70,30 @@
         Next
     End Sub
 
-    Function getFeasibleRequests(ByRef previousMove As SolutionStep) As List(Of Tuple(Of Integer, Integer))
-        Dim feasibleMoves As List(Of Tuple(Of Integer, Integer)) = New List(Of Tuple(Of Integer, Integer))
-        Dim tmp As Tuple(Of Integer, Integer) = New Tuple(Of Integer, Integer)(0, 0)
-        feasibleMoves.Add(tmp)
-        Return feasibleMoves
+    Function getFeasibleRequests(ByRef previousStep As SolutionStep) As List(Of Tuple(Of Integer, Integer))
+        Dim feasibleRequests As List(Of Tuple(Of Integer, Integer)) = New List(Of Tuple(Of Integer, Integer))
+        ' Dim tmp As Tuple(Of Integer, Integer) = New Tuple(Of Integer, Integer)(0, 0)
+        Dim movementDistance As Integer = 1
+        While True 'Movement loop, solange bis unzulässig
+            ' Prüfe ob nicht bediente Requests vorhanden
+            If previousStep.servedRequests.Equals(Phi) Then
+                Exit While ' Alles bedient
+            End If
+            ' Prüfe, ob ein Request zulässig ist oder ob breits zu weit gelaufen
+            For Each request In Phi
+                If previousStep.servedRequests.Contains(request) Then
+                    Continue For ' Schon bedient
+                End If
+                Dim evalPosition As Integer = previousStep.kundenPositionen(request.Item1) + movementDistance
+                If Requests(request.Item1)(request.Item2) = evalPosition Then
+                    feasibleRequests.Add(request) ' Am richtigen Tresen
+                ElseIf Requests(request.Item1)(request.Item2) < evalPosition Then
+                    Exit While ' Zu weit gelaufen
+                End If
+            Next
+            movementDistance += 1 ' eins weiter
+        End While
+        Return feasibleRequests
     End Function
 
     Sub addStepTime(ByRef request As Tuple(Of Integer, Integer),
